edcae1891de7ac8a4f0d4db4699d36bac72940ba
mudassir-jmi <mudassir7991@gmail.com>
2025-02-03 12:30:24 +0530

    ✨ Added: src\example.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-import { createFileTracker } from 'git-file-tracker';
+const { createFileTracker } = require('git-file-tracker');
 
+// Create a file tracker instance
 const tracker = createFileTracker({
-    watchPath: './src',
-    autoCommit: true,
-    autoPush: true,
-    branch: 'main'
+    watchPath: './src',          // Change this to watch different directories
+    gitDir: process.cwd(),       // Change the Git repository location
+    ignorePaths: ['*.log'],      // Add patterns to ignore
+    autoCommit: true,            // Set to false to disable automatic commits
+    autoPush: false,             // Set to false to disable automatic pushing
+    branch: 'main'        // Change the target branch
 });
 
-// The tracker will automatically monitor files and create commits
-// To stop tracking:
-await tracker.stop();
+console.log('File tracker started! Try making some changes in the src directory.');
+
+// Handle process termination
+process.on('SIGINT', async () => {
+    console.log('\nStopping file tracker...');
+    await tracker.stop();
+    process.exit(0);
+});
---
+++ src/example.js
@@ -0,0 +1,6 @@
+// This is an example file that will be tracked
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+module.exports = { greet };
